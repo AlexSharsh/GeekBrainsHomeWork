3b26120b040d3d19b35843f1a87a06c71a0305d6
a.sharshukov <a.sharshukov@atol.ru>
2022-02-12 22:33:38 +0300

    HomeWork6

---
+++ HomeWork/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkStudent
+{
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public string university;
+        int age;
+        public int course;
+
+        // Создаем конструктор
+        public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
+        {
+            this.name = lastName;
+            this.surname = firstName;
+            this.university = university;
+            this.age = age;
+            this.course = course;
+        }
+    }
+}
